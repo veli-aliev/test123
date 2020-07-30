076628c2a2df924102ca99049dc1ff4011abafdb
Ali <alialiev10@yandex.ru>
2020-07-30 23:03:57 +0300

    msg

---
+++ precache-manifest.eea5bfd25b1a0b2acaf15d38a741672a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857591bf55adc186a59bb0717e521aeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79e22b95a6c6ed6ece9f",
+    "url": "/static/css/main.e661e4fa.chunk.css"
+  },
+  {
+    "revision": "4e8a9241ed04ad5b8c49",
+    "url": "/static/js/2.7ea89dba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7ea89dba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79e22b95a6c6ed6ece9f",
+    "url": "/static/js/main.83176120.chunk.js"
+  },
+  {
+    "revision": "1d4b349ed56d3a94a831",
+    "url": "/static/js/runtime-main.e5e36c01.js"
+  }
+]);
